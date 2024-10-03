301008c98d63208d5c66a32e08828a19b611fd36
TerryTan <yisyuntan@gmail.com>
2024-10-03 21:28:59 +0800

    lesson 3-31

--- try.py
+++ try.py
@@ -1,4 +1,11 @@
-s = set()
-s.add(1)
-s.add(3)
-print(s)
+a = {1, 3, 4, 5}
+b = {3, 4, 7, 8}
+
+print(a.difference(b))
+print(b.difference(a))
+print(a.intersection(b))
+print(b.intersection(a))
+print(a.union(b))
+print(b.union(a))
+
+print(a.isdisjoint(b))
