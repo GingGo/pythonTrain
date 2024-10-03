e6ebf7a504b7a34446b427d858abe28b5a4716d1
TerryTan <yisyuntan@gmail.com>
2024-10-03 11:13:50 +0800

    lesson 3-13

--- try.py
+++ try.py
@@ -1,22 +1,3 @@
-import math
-
-print(abs(-10))
-print(abs(10))
-
-print(pow(2, 10))
-
-print(max(1, 2, 4, 5, 10))
-
-print(round(2.5))
-print(round(3.5))
-# x.5, the values will be rounded up if the roundup value is an odd number
-# otherwise, it will be rounded down
-
-
-print(int(3.6))
-
-print(str(33) + str(44))
-
-
-print(math.e)
-print(math.pi)
+x = 5
+x = x + 1
+print(x)
