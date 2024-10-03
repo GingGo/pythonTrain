8559ed55918d07c52a0040f058ce156bfd93c7c9
TerryTan <yisyuntan@gmail.com>
2024-10-03 21:08:17 +0800

    lesson 3-30

--- try.py
+++ try.py
@@ -1,9 +1,4 @@
-a = ([1, 2, 3], "Terry")
-a[0][1] = 100
-print(a)
-
-# if an element in a tuple is mutable, then we can just
-# select the element, and then change it.
-
-# If we want to use a tuple as a dictionary key, then
-# all elements in the tuple has to be immutable.
+s = set()
+s.add(1)
+s.add(3)
+print(s)
