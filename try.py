1b299e5732b4f6075ca2f78224805953613d0d1d
TerryTan <yisyuntan@gmail.com>
2024-10-03 14:31:19 +0800

    lesson 3-18

--- try.py
+++ try.py
@@ -1,9 +1,17 @@
-print(len("Test"))
-print(int(455))
-print("test".upper())
-print("AAA".lower())
-
 name = "Terry"
-print(name.upper().isupper())
+print(name.replace("rr", "xxx"))
 
-print(name.index("rr"))
+sentence = "Today is a bad day."
+print(sentence.split(" "))
+
+print(list(sentence))
+
+print("I have a format {}".format(["aaa", 16]))
+
+print("{0}, {1}, {1}".format(10, "string", 3.14))
+
+print("{name}, {addr}, {age}".format(name="tery", addr=25, age=17))
+
+a = "stra"
+age12 = 44
+print(f"example is {a}, my age = {age12}")
