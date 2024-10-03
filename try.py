991d125b5c0c3354a120e643e663adb7846cc6ab
TerryTan <yisyuntan@gmail.com>
2024-10-03 16:42:54 +0800

    lesson 3-25

--- try.py
+++ try.py
@@ -1,11 +1,4 @@
 person = {"name": "Terry", "age": 25}
-print(person["name"])
-
-person2 = {"x": {"age": [10, 20, 30]}}
-
-print(person2["x"]["age"][2])
-
-te = {}
-te["name"] = "Jason"
-te["age"] = 40
-print(te)
+print(person.keys())
+print(person.values())
+print(person.items())
