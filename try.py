f4aad7c69e12b9595faf628450c4ab00287a401a
TerryTan <yisyuntan@gmail.com>
2024-10-03 14:41:44 +0800

    lesson 3-19

--- try.py
+++ try.py
@@ -1,17 +1,7 @@
+print("Today is a bad day".count('a'))
+
 name = "Terry"
-print(name.replace("rr", "xxx"))
 
-sentence = "Today is a bad day."
-print(sentence.split(" "))
+print(name.startswith("T"))
 
-print(list(sentence))
-
-print("I have a format {}".format(["aaa", 16]))
-
-print("{0}, {1}, {1}".format(10, "string", 3.14))
-
-print("{name}, {addr}, {age}".format(name="tery", addr=25, age=17))
-
-a = "stra"
-age12 = 44
-print(f"example is {a}, my age = {age12}")
+print(name.endswith("try"))
