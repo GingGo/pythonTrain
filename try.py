c81c95c02faf42be0b2301be4e8e2b20f4545541
TerryTan <yisyuntan@gmail.com>
2024-10-04 22:05:18 +0800

    lesson 5-42

--- try.py
+++ try.py
@@ -1,12 +1,18 @@
-if False:
-    print("This is True")
+# program asks user's name
+# cash
+# Y/N
+# program checks if the user has more than or equal to $30
+
+name = input("Enter your name:")
+money = input("Enter your cash amount:")
+hungry = input("Are you hungry? (Y/N)")
+
+if hungry == "Y":
+    if int(money) >= 30:
+        print(f"{name} should go eat breakfast")
+    else:
+        print("Your money is not enough")
+elif hungry == "N":
+    print("You are not hungry")
 else:
-    print("This is false")
-
-
-age = 5
-
-if (age < 8):
-    print("movie is free for you")
-else:
-    print("You need to pay $300")
+    print("Please make sure that you enter either Y or N")
