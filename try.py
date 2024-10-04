465f84ae19db6abe9b9af79bd66d75635810efdb
TerryTan <yisyuntan@gmail.com>
2024-10-04 22:37:04 +0800

    lesson 5-43

--- try.py
+++ try.py
@@ -1,18 +1,9 @@
-# program asks user's name
-# cash
-# Y/N
-# program checks if the user has more than or equal to $30
+getStr = input("Enter input:")
 
-name = input("Enter your name:")
-money = input("Enter your cash amount:")
-hungry = input("Are you hungry? (Y/N)")
-
-if hungry == "Y":
-    if int(money) >= 30:
-        print(f"{name} should go eat breakfast")
-    else:
-        print("Your money is not enough")
-elif hungry == "N":
-    print("You are not hungry")
-else:
-    print("Please make sure that you enter either Y or N")
+match getStr:
+    case "a" | "t":
+        print("Input a or t")
+    case "b":
+        print("Input b")
+    case _:
+        print("Other Input")
