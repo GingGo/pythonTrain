554722a37c55189e6b9e1815d0e217b1bcbfefc0
TerryTan <yisyuntan@gmail.com>
2024-10-05 15:11:01 +0800

    lesson 5-53

--- try.py
+++ try.py
@@ -1,10 +1,26 @@
-x = [1, 2, 3, 4]
-y = {item: item ** 2 for item in x}
-print(y)
+from sys import argv
 
-z_set = {item ** 2 for item in x}
-print(z_set)
+# print(argv)
+if len(argv) < 2:
+    print("Please provide a filename")
+else:
+    file = open(argv[1])
+    print(file)
+    lines = file.read()
 
-x_sqr_gener = (item ** 2 for item in x)
-for item in x_sqr_gener:
-    print(item)
+    print(type(lines))
+    print(lines)
+    lines = lines.split("\n")
+    line_count = len(lines)
+    word_count = 0
+    letter_count = 0
+    for line in lines:
+        words = line.split(" ")
+        word_count += len(words)
+        letter_count += len(line)
+
+    print(lines)
+
+    print("line count:", line_count)
+    print("word count:", word_count)
+    print("letter count:", letter_count)
