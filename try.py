732c6d9c11ec35debd1d9766f3a854a88cb3851f
TerryTan <yisyuntan@gmail.com>
2024-10-06 01:04:54 +0800

    lesson 6-60

--- try.py
+++ try.py
@@ -1,6 +1,25 @@
-def sum(n1, n2=0):
-    return n1 + n2
+def sum(*args):
+    ret = 0
+    print(args)
+    print(type(args))
+    for i in range(len(args)):
+        ret += args[i]
+    return ret
 
 
-print(sum(12, 25))
-print(sum(55))
+print(sum(1, 2, 3, 4, 5, 6, 7, 8))
+
+
+def myFunc(**kwargs):
+    print(kwargs)
+    print(type(kwargs))
+
+
+myFunc(name="Terry", age=25, addr="Taipei")
+
+
+def myFunc2(*args, **kwargs):
+    print("I would like to eat {} {}".format(args[1], kwargs["food"]))
+
+
+myFunc2(1, 5, 10, "Hello", name="Terry", food="eggs")
