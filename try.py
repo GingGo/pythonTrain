f33b202fa30a329b201a93afcb7d62ab81d85e60
TerryTan <yisyuntan@gmail.com>
2024-10-05 11:27:23 +0800

    lesson 5-45

--- try.py
+++ try.py
@@ -1,9 +1,4 @@
-for letter in "Hello World":
-    print(letter)
-
-for tuple in [(1, 2), (3, 5), (7, 9)]:
-    print(tuple)
-
-
-for a, b in [(1, 2), (3, 5), (7, 9)]:
-    print(a, b)
+x = 0
+while x < 5:
+    print(x)
+    x += 1
