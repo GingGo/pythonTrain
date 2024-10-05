b41901803c93a8b216dc806b8e1f8959ea0b7bd7
TerryTan <yisyuntan@gmail.com>
2024-10-05 20:57:12 +0800

    lesson 6-54

--- try.py
+++ try.py
@@ -1,26 +1,12 @@
-from sys import argv
+def sayHi():
+    print("Hello, how are you")
 
-# print(argv)
-if len(argv) < 2:
-    print("Please provide a filename")
-else:
-    file = open(argv[1])
-    print(file)
-    lines = file.read()
 
-    print(type(lines))
-    print(lines)
-    lines = lines.split("\n")
-    line_count = len(lines)
-    word_count = 0
-    letter_count = 0
-    for line in lines:
-        words = line.split(" ")
-        word_count += len(words)
-        letter_count += len(line)
+sayHi()
 
-    print(lines)
 
-    print("line count:", line_count)
-    print("word count:", word_count)
-    print("letter count:", letter_count)
+def addition(x, y, z):
+    print(x+y+z)
+
+
+addition(1, 6, 8)
