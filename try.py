949b28b08ef863a98a27cf9ecc1a596e3504328a
TerryTan <yisyuntan@gmail.com>
2024-10-06 01:33:51 +0800

    lesson 6-62

--- try.py
+++ try.py
@@ -1,35 +1,5 @@
-def higherOrder(fn):
-    fn()
+ret = (lambda x: x**2)(5)
+print(ret)
 
-
-def smallFunc():
-    print("Hello from small function.")
-
-
-higherOrder(smallFunc)
-
-
-def square(num):
-    return num ** 2
-
-
-myList = [-10, 3, 9, 8, 10]
-
-
-print(map(square, myList))
-
-for item in map(square, myList):
-    print(item)
-
-print("-----------------------------------")
-
-
-def even(num):
-    if num % 2 == 0:
-        return True
-
-
-evencheckList = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-
-for i in filter(even, evencheckList):
-    print(i)
+mytuple = (lambda x, y: (x+y, x-y))(1, 6)
+print(mytuple)
