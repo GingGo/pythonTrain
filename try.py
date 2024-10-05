d1a55dcd03d9213d23b8408e775de90994ef8f06
TerryTan <yisyuntan@gmail.com>
2024-10-05 12:00:45 +0800

    lesson 5-47

--- try.py
+++ try.py
@@ -1,3 +1,17 @@
-for i in "1234":
-    for j in "abcdefghijk":
-        print(i, j)
+for i in "How are you":
+    pass
+
+print("after pass")
+
+
+for i in "12345678":
+    if int(i) > 5:
+        break
+    else:
+        print(i)
+
+print("-------------------------------")
+for i in "ABCDEFG":
+    if i == "D":
+        continue
+    print(i)
