5628413884fdc77f8f786efc8677a997e0f98683
TerryTan <yisyuntan@gmail.com>
2024-10-05 14:50:39 +0800

    lesson 5-52

--- try.py
+++ try.py
@@ -1,3 +1,10 @@
 x = [1, 2, 3, 4]
-y = [i ** 2 for i in x if i < 3]
+y = {item: item ** 2 for item in x}
 print(y)
+
+z_set = {item ** 2 for item in x}
+print(z_set)
+
+x_sqr_gener = (item ** 2 for item in x)
+for item in x_sqr_gener:
+    print(item)
