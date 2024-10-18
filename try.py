49a79aad3c1a9ca1c539c93fed71a905cf7e7b01
TerryTan <yisyuntan@gmail.com>
2024-10-18 21:01:17 +0800

    Lesson 10_87

--- try.py
+++ try.py
@@ -1,2 +1,25 @@
-import os
-os.remove("index.html")
+# user_input = input("How old are you?")
+# print("---------------------------------")
+# print(user_input)
+# print(type(user_input))
+
+import random
+
+sercret_num = random.randint(1, 100)
+v_min = 1
+v_max = 100
+while True:
+    user_input = input(f"key a num between {v_min} and {v_max}:")
+    if int(user_input) < v_min or int(user_input) > v_max:
+        print("input fail!")
+        continue
+
+    if int(user_input) == sercret_num:
+        print(f"right {sercret_num}")
+        break
+
+    if int(user_input) < sercret_num:
+        v_min = int(user_input)
+
+    if int(user_input) > sercret_num:
+        v_max = int(user_input)
