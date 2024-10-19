06a82ed802ec4c6038d23954cf8313b559748800
TerryTan <yisyuntan@gmail.com>
2024-10-19 15:11:10 +0800

    Lesson 11_104

--- try.py
+++ try.py
@@ -1,12 +1,20 @@
-import pickle
+class Employee:
+    def __init__(self):
+        self.income = 0
+        self.__tax = 0
 
-# x = 10
-# y = [1, 2, 3, 4]
+    def earn_money(self, money):
+        self.income += money
+        self.__tax += self.income * 0.05
 
-# with open("pickle_file", mode="wb") as p_file:
-#     pickle.dump(x, p_file)
-#     pickle.dump(y, p_file)
+    def get_tax(self):
+        return self.__tax
 
-with open("pickle_file", mode="rb") as p_file:
-    print(pickle.load(p_file))
-    print(pickle.load(p_file))
+    @property
+    def tax_amount(self):
+        return self.income * 0.05
+
+
+Terry = Employee()
+Terry.earn_money(300)
+print(Terry.tax_amount)
