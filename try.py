b9ae72c6399f223cd5125eeefedc28889cc5989c
TerryTan <yisyuntan@gmail.com>
2024-10-21 21:05:41 +0800

    Lesson 13_116

--- try.py
+++ try.py
@@ -1,21 +1,11 @@
-class Robot:
-    def __init__(self, name, age, address):
-        self.name = name
-        self.age = age
-        self.address = address
-
-    def __key(self):
-        return (self.name, self.age, self.address)
-
-    def __hash__(self):
-        return hash(self.__key())
-
-    def __eq__(self, other):
-        if isinstance(other, Robot):
-            return self.__key() == other.__key()
-        return NotImplemented
+def safe_dvide(x, y):
+    try:
+        return x / y
+    except ZeroDivisionError:
+        print("Divide by 0.")
+        return None
 
 
-robot = Robot("Terry", 25, "Taiwan")
-robot1 = Robot("Terry", 25, "Taiwan")
-print(robot == robot1)
+a = int(input("Enter a:"))
+b = int(input("Enter b:"))
+print(f"a / b = {safe_dvide(a, b)}")
