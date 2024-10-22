cda985a592ebab5d88c2d808f5dccb66d9d04d96
TerryTan <yisyuntan@gmail.com>
2024-10-22 22:29:36 +0800

    Lesson 13_121

--- try.py
+++ try.py
@@ -1,15 +1,3 @@
-def divide(a, b):
-    if type(a) != int or type(b) != int:
-        raise ValueError("Not vaild type given!")
-
-    if b == 0:
-        raise ZeroDivisionError("Second argument can't be 0")
-
-    return a / b
-
-
-try:
-    print(divide(10, "hello"))
-
-except Exception as e:
-    print(e)
+with open("test.txt") as te:
+    data = te.read()
+    print(data)
